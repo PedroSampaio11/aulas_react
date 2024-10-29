23a607005dbed84ca134d9e5061bb7fe47f278f5
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-10-29 18:25:05 -0300

    uppp

--- 2_Fundamentos/fundamentos/src/components/FIrstComponent.jsx
+++ 2_Fundamentos/fundamentos/src/components/FIrstComponent.jsx
@@ -1,10 +1,10 @@
 const FirstComponent = () => {
   const name = "pedro"
   return(
-    <div>
+    <>
       <h2 className="tests"> Ola, {name} </h2>
       <p> seja bem vindo ao react.js </p>
-    </div>
+    </>
   )
 }
 
