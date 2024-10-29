e077afe94283d883d2378ac6efbc6c882144f8a7
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-10-29 18:21:21 -0300

    first commit

---
+++ 2_Fundamentos/fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import "./App.css";
+import FirstComponent from "./components/FIrstComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> meu hello world em jsx</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_Fundamentos/fundamentos/src/components/FIrstComponent.jsx
@@ -0,0 +1,10 @@
+const FirstComponent = () => {
+  const name = "pedro"
+  return(
+    <div>
+      <h2 className="tests"> Ola, {name} </h2>
+    </div>
+  )
+}
+
+export default FirstComponent;
