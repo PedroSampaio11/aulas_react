53eed6fdf569dc47e33244554501baabcbd2966d
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-11-07 16:15:49 -0300

    add

---
+++ 3_Avançando/avançando/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1> teste </h1>
+      {/* Imagem em public */}
+      <img src="./img1.jpg" alt="" />
+    </div>
+  )
+}
+
+export default App
