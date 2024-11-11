0280583ff4d4175b67516a56dc7a67fc0dac68cb
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-11-11 16:54:02 -0300

    "Removed img2.png from public folder, updated App.css to change img height and added object-fit, updated App.jsx to import img2 from assets and add it to the component, and removed react.svg from assets folder."

--- 3_Avançando/avançando/src/App.jsx
+++ 3_Avançando/avançando/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-
+//  importando imagem
+import img2 from "./assets/img2.png"
 function App() {
 
   return (
     <div className='App'>
       <h1> teste </h1>
       {/* Imagem em public */}
+      <h2> Imagem em public</h2>
       <img src="./img1.jpg" alt="" />
+      {/* imagem em assets */}
+      <h2> Imagem em assets</h2>
+      <img src={img2} alt="" />
     </div>
   )
 }
