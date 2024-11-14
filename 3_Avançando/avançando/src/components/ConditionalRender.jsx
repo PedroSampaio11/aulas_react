d0f602645b4ada5c63f7b1d2f3e911d3741a4c7b
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-11-14 15:37:25 -0300

    Added new variable `nome` and a conditional rendering example using a ternary operator.

--- 3_Avançando/avançando/src/components/ConditionalRender.jsx
+++ 3_Avançando/avançando/src/components/ConditionalRender.jsx
@@ -1,9 +1,14 @@
 const ConditionalRender = () => {
   const x = 5;
+  const nome = "pedro"
   return (
     <div>
+      {/* Apenas uma opçao */}
       <h3> Isso será exibido? </h3>
       {x <= 5 && <p>sim, o número é: {x} </p>}
+      {/* ternário */}
+      <h3> Validação com operador ternário:</h3>
+      { nome === "pedro" ? ( <p> Olá {nome}</p>) : ( <p>Usuário nao encontrado!</p>)}
     </div>
   );
 };
